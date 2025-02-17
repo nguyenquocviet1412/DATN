0222eed6d9f06f089299a0c9915410ec21595258
nguyenquocviet1412 <vietvv1412@gmail.com>
2025-02-17 20:53:07 +0700

    giao diện và route product

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -30,7 +30,7 @@
 
 
       <!-- User Menu-->
-      <li><a class="app-nav__item" href="{{route('logout')}}"><i class='bx bx-log-out bx-rotate-180'></i> </a>
+      <li><a class="app-nav__item" href=""><i class='bx bx-log-out bx-rotate-180'></i> </a>
 
       </li>
     </ul>
@@ -62,23 +62,23 @@
     <hr>
     <ul class="app-menu">
 
-      <li><a class="app-menu__item " href="{{route('admin')}}"><i class='app-menu__icon bx bx-tachometer'></i><span
+      <li><a class="app-menu__item " href=""><i class='app-menu__icon bx bx-tachometer'></i><span
             class="app-menu__label">Bảng điều khiển</span></a></li>
-      <li><a class="app-menu__item " href="{{route('admin.nhanvien')}}"><i class='app-menu__icon bx bx-id-card'></i> <span
+      <li><a class="app-menu__item " href=""><i class='app-menu__icon bx bx-id-card'></i> <span
             class="app-menu__label">Quản lý nhân viên</span></a></li>
-      <li><a class="app-menu__item" href="{{route('admin.khachhang')}}"><i class='app-menu__icon bx bx-user-voice'></i><span
+      <li><a class="app-menu__item" href=""><i class='app-menu__icon bx bx-user-voice'></i><span
             class="app-menu__label">Quản lý khách hàng</span></a></li>
-      <li><a class="app-menu__item" href="{{route('admin.sanpham')}}"><i
+      <li><a class="app-menu__item" href=""><i
             class='app-menu__icon bx bx-purchase-tag-alt'></i><span class="app-menu__label">Quản lý sản phẩm</span></a>
       </li>
-      <li><a class="app-menu__item" href="{{route('admin.donhang')}}"><i class='app-menu__icon bx bx-task'></i><span
+      <li><a class="app-menu__item" href=""><i class='app-menu__icon bx bx-task'></i><span
             class="app-menu__label">Quản lý đơn hàng</span></a></li>
-      <li><a class="app-menu__item" href="{{route('admin.noibo')}}"><i class='app-menu__icon bx bx-run'></i><span
+      <li><a class="app-menu__item" href=""><i class='app-menu__icon bx bx-run'></i><span
             class="app-menu__label">Quản lý nội bộ
           </span></a></li>
-      <li><a class="app-menu__item" href="{{route('salaries.index')}}"><i class='app-menu__icon bx bx-dollar'></i><span
+      <li><a class="app-menu__item" href=""><i class='app-menu__icon bx bx-dollar'></i><span
             class="app-menu__label">Bảng kê lương</span></a></li>
-      <li><a class="app-menu__item" href="{{route('doanhthu.index')}}"><i
+      <li><a class="app-menu__item" href=""><i
             class='app-menu__icon bx bx-pie-chart-alt-2'></i><span class="app-menu__label">Báo cáo doanh thu</span></a>
       </li>
       <li><a class="app-menu__item" href="page-calendar.html"><i class='app-menu__icon bx bx-calendar-check'></i><span
