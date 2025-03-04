8afd1bc6ec9c92a646819a9a797756d559790943
nguyenquocviet1412 <vietvv1412@gmail.com>
2025-03-04 23:45:45 +0700

    đăng nhập đăng xuất cho trang quản trị

---
+++ app/Http/Controllers/admin/EmployeeAuthController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers\admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Employee;
+
+class EmployeeAuthController extends Controller
+{
+    public function getLogin()
+    {
+        return view('admin.employee_login');
+    }
+
+    public function postLogin(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::guard('employee')->attempt($credentials)) {
+            $employee = Auth::guard('employee')->user();
+
+            // Lưu toàn bộ thông tin của nhân viên vào session
+            session(['employee' => $employee]);
+            return redirect()->route('admin.dashboard');
+        }
+
+        return back()->with('message', 'Email hoặc mật khẩu không chính xác.');
+    }
+
+    //Đăng xuất tài khoản quản trị viên
+    public function logout()
+{
+    // Xóa session
+    session()->forget('employee');
+
+    // Đăng xuất tài khoản
+    Auth::guard('employee')->logout();
+
+    // Chuyển hướng về trang đăng nhập kèm thông báo
+    return redirect()->route('admin.login')->with('logout_success', true);
+}
+}
