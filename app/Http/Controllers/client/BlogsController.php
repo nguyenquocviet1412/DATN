d1da0deee879618a09987a2be403a7b1f001700c
nguyenquocviet1412 <vietvv1412@gmail.com>
2025-03-23 23:01:33 +0700

    sửa lại link ảnh và giao dienj trang blog

--- app/Http/Controllers/client/BlogsController.php
+++ app/Http/Controllers/client/BlogsController.php
@@ -9,30 +9,38 @@ use Illuminate\Support\Facades\Storage;
 
 class BlogsController extends Controller
 {
-    //
     public function index(Request $request)
-    {
-        //
-        //
-        $sortBy = $request->input('sort_by', 'id');
-        $sortOrder = $request->input('sort_order', 'asc');
-        $search = $request->input('search');
+{
+    $sortBy = $request->input('sort_by', 'id');
+    $sortOrder = $request->input('sort_order', 'asc');
+    $search = $request->input('search');
+    $monthFilter = $request->input('month_filter'); // Lọc theo tháng-năm
 
-        $query = Post::query();
+    $query = Post::query();
 
-        if ($search) {
-            $query->where('title', 'like', "%$search%");
-        }
-
-        $posts = $query->orderBy($sortBy, $sortOrder)->paginate(10);
-        
-        return view('blogs.index', compact('posts', 'sortBy', 'sortOrder', 'search'));
+    // Tìm kiếm theo tiêu đề bài viết
+    if ($search) {
+        $query->where('title', 'like', "%$search%");
     }
+
+    // Lọc theo tháng & năm
+    if ($monthFilter) {
+        $query->whereYear('created_at', '=', date('Y', strtotime($monthFilter)))
+              ->whereMonth('created_at', '=', date('m', strtotime($monthFilter)));
+    }
+
+    // Phân trang bài viết
+    $posts = $query->orderBy($sortBy, $sortOrder)->paginate(10);
+
+    // Lấy 3 bài viết mới nhất
+    $latestPosts = Post::orderBy('created_at', 'desc')->limit(3)->get();
+
+    return view('blogs.index', compact('posts', 'latestPosts', 'sortBy', 'sortOrder', 'search', 'monthFilter'));
+}
+
     public function details(string $id)
     {
-        //
-        //
         $post = Post::findOrFail($id);
-        return view('blogs.details',compact('post'));
+        return view('blogs.details', compact('post'));
     }
 }
