3b1ed6b3ad4020157809f03420ac81b880064e14
tukyx77 <tuddaph43076@fpt.edu.vn>
2025-03-29 23:53:39 +0700

    Sửa lại bộ lọc

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -24,6 +24,11 @@ class ShopController extends Controller
         // Truy vấn sản phẩm dựa trên biến thể
         $products = Product::whereHas('variants', function ($query) use ($request) {
 
+            if ($request->filled('search')) {
+                $products = Product::where('name', 'like', '%' . $request->search . '%');
+            } else {
+                $products = Product::query();
+            }
             // Lọc theo màu sắc
             if ($request->filled('id_color')) {
                 $selectedColors = is_array($request->id_color) ? $request->id_color : [$request->id_color];
@@ -45,11 +50,6 @@ class ShopController extends Controller
         // Lọc theo khoảng giá
         if ($request->filled('max_price') && is_numeric($request->max_price)) {
             $products->where('price', '<=', (int) $request->max_price);
-        }
-
-        // Tìm kiếm theo từ khóa
-        if ($request->filled('search')) {
-            $products->where('name', 'like', '%' . $request->search . '%');
         }
 
         // Lọc theo điểm đánh giá trung bình
@@ -78,7 +78,7 @@ class ShopController extends Controller
                 $products->orderBy('created_at', 'desc'); // Mặc định: sản phẩm mới nhất
                 break;
         }
-        
+
         // Trả về danh sách sản phẩm
         $products = $products->with('rates')->distinct()->paginate(9)->appends(request()->query());
 
